8ab121abc4299cfb9181db542c5c02d9e8424d84
T0114411@ciber.local <T0114411@ciber.local>
2019-01-11 22:45:19 +0000

    11-01-2019 A-H

--- FT01/FT01/Program.cs
+++ FT01/FT01/Program.cs
@@ -10,8 +10,79 @@ namespace FT01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ////Exercicio A
+            //string nome;
+            //Console.Write("Insira o seu nome:");
+            //nome = Console.ReadLine();
+            //Console.Write("Olá "+nome+"!");
+            ////Exercicio B
+            //if(nome.ToLower().Equals("bartolomeu")) {
+            //    Console.Write("Dá cá o meu!");
+            //}
+            ////Exercicio C
+            //if (nome.ToLower().Substring(nome.Length - 2, 2).Equals("eu")){
+            //    Console.Write("Dá cá o meu!");
+            //}
+            ////Exercicio D
+            //string nome_C;
+            //Console.Write("Insira o seu nome completo:");
+            //nome_C = Console.ReadLine();
+            //string[] nomes;
+            //nomes = nome_C.Split(' ');
+            //Console.WriteLine("Primeiro nome: "+ nomes[0]);
+            //Console.WriteLine("Ultimo nome: " + nomes[nomes.Length-1]);
+            ////Exercicio E
+            //string[] frases= new string[3];
+            //for(int i=0;i<3;i++) {
+            //    Console.WriteLine("Frase numero " + (i + 1) + " :");
+            //    frases[i]=Console.ReadLine();
+            //}
+            //int maior = 0;
+            //for(int i = 0; i < 3; i++){
+            //    if(frases[maior].Length<frases[i].Length)
+            //        maior=i;
+            //}
+            //Console.WriteLine("Maior Frase: "+(maior + 1));
+            //Console.WriteLine("'" + frases[maior] + "'");
+            ////Exercicio F
+            //string[] teste1 = { "teste", "testezao", "testezinho" , "porra" };
+            //string[] teste2 = { "teste", "papel", "patos", "quack" };
+            //if (exercicioF(teste1))
+            //    Console.WriteLine("Mesmo numero de letras");
+            //else
+            //    Console.WriteLine("Numero diferente de letras");
+            //if (exercicioF(teste2))
+            //    Console.WriteLine("Mesmo numero de letras");
+            //else
+            //    Console.WriteLine("Numero diferente de letras");
+            ////Exercicio G
+            //string[] teste2 = { "teste", "papel", "patos", "quack" };
+            //Console.WriteLine("Resultado: " + exercicioG(teste2));
+            //Exercicio H
+            string teste;
+            Console.Write("Insira a frase:");
+            teste = Console.ReadLine();
+            teste=teste.Replace('v', 'b').Replace('V', 'B');
+            teste=teste.Replace("ão", "om").Replace("ÃO", "OM");
+            Console.WriteLine(teste);
+
             Console.ReadKey();
+        }
+
+        public static bool exercicioF(string[] frase) {
+            int t = frase[0].Length;
+            for(int i = 1; i < frase.Length; i++) {
+                if(t!=frase[i].Length) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static int exercicioG(string[] frase) {
+            int soma = 0;
+            for (int i = 0; i < frase.Length; i++)
+                soma += frase[i].Length;
+            return soma;
         }
     }
 }
