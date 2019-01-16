c2200a4395ebf99a4071d65ff0d724481d51d919
Mikas-Atec <miguel.pereira.t0114411@edu.atec.pt>
2019-01-16 20:46:49 +0000

    Grande quiz

--- FT01/FT01/Program.cs
+++ FT01/FT01/Program.cs
@@ -65,7 +65,90 @@ namespace FT01
             //teste=teste.Replace('v', 'b').Replace('V', 'B');
             //teste=teste.Replace("ão", "om").Replace("ÃO", "OM");
             //Console.WriteLine(teste);
-            //Exercicio I
+            ////Exercicio I
+            //string n;
+            //Console.WriteLine("Digite a sua frase:");
+            //n = Console.ReadLine();
+            //if (exercicioI(n))
+            //    Console.Write("É composto somente por algarismos.");
+            //else
+            //    Console.Write("Não é composto somente por algarismos.");
+            ////Exericico J
+            //string n;
+            //Console.WriteLine("Digite a sua frase:");
+            //n = Console.ReadLine();
+            //if (exercicioJ(n))
+            //    Console.Write("A frase não contem algarismos.");
+            //else
+            //    Console.Write("A frase contem algarismos.");
+            ////Exercicio K
+            //string s1, s2;
+            //Console.Write("Digite a sua frase 1:");
+            //s1 = Console.ReadLine();
+            //Console.Write("Digite a sua frase 2:");
+            //s2 = Console.ReadLine();
+            //if (string.Compare(s1.ToLower(), s2.ToLower()) > 0)
+            //    Console.WriteLine("Frase 2 esta em primeiro");
+            //else if (string.Compare(s1.ToLower(), s2.ToLower()) < 0)
+            //    Console.WriteLine("Frase 1 esta em primeiro");
+            //else
+            //    Console.WriteLine("As frases são iguais");
+            //Exericio L
+            //string[] frases = new string[10];
+            //for(int i=0;i<10;i++)
+            //{
+            //    Console.Write("Insira a frase Nº" + (i + 1) + ":");
+            //    frases[i] = Console.ReadLine();
+            //}
+            //frases = exercicioL(frases);
+            //for (int i = 0; i < frases.Length; i++)
+            //    Console.WriteLine(frases[i]);
+            //Exercicio M
+            /*
+             *  0 = Questao
+             *  1 = Resposta certa
+             *  2 = Resposta 1
+             *  3 = Resposta 2
+             *  4 = Resposta 3
+             *  5 = Resposta 4
+             */
+            //string[,] questoes = new string[10, 6];
+            //string resposta;
+            //int valido;
+
+            //for(int i=0;i<10;i++)
+            //{
+            //    questoes[i, 0] = "Pergunta generica com resposta " + ((i % 4) + 1);
+            //    questoes[i, 1] = ((i % 4) +1).ToString();
+            //    questoes[i, 2] = "Pergunta generica 1";
+            //    questoes[i, 3] = "Pergunta generica 2";
+            //    questoes[i, 4] = "Pergunta generica 3";
+            //    questoes[i, 5] = "Pergunta generica 4";
+            //}
+
+            //for(int i=0;i<10;i++)
+            //{
+            //    valido = 0;
+            //    Console.WriteLine(questoes[i,0]);
+            //    for (int j = 1; j <= 4; j++)
+            //        Console.WriteLine(j+" - "+questoes[i,j+1]);
+            //    while(valido==0)
+            //    {
+            //        Console.WriteLine("Qual a sua resposta");
+            //        resposta = Console.ReadLine();
+            //        if(!string.IsNullOrWhiteSpace(resposta))
+            //        {
+            //            if (int.Parse(resposta) > 0 && int.Parse(resposta) <= 4)
+            //            {
+            //                valido = 1;
+            //                if (int.Parse(resposta) == int.Parse(questoes[i, 1]))
+            //                    Console.WriteLine("Respondeu certo!");
+            //                else
+            //                    Console.WriteLine("Respondeu errado.");
+            //            }
+            //        }
+            //    }
+            //}
 
             Console.ReadKey();
         }
@@ -84,6 +167,50 @@ namespace FT01
             for (int i = 0; i < frase.Length; i++)
                 soma += frase[i].Length;
             return soma;
+        }
+
+        public static bool exercicioI(string frase) {
+            char[] numeros = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int i=0;i<frase.Length;i++) {
+                if (!numeros.Contains(frase[i]))
+                        return false;
+            }
+            return true;
+        }
+
+        public static bool exercicioJ(string frase)
+        {
+            char[] numeros = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            for (int i = 0; i < frase.Length; i++)
+            {
+                if (numeros.Contains(frase[i]))
+                    return false;
+            }
+            return true;
+        }
+
+        public static string[] exercicioL(string[] frases)
+        {
+            if(frases.Length!=10)
+            {
+                string[] erro = { "ERROR","Numero de frases invalido" };
+                return erro;
+            }
+            string aux;
+            for(int i=0;i<10;i++)
+            {
+                for(int j=i;j<10;j++)
+                {
+                    if(string.Compare(frases[i].ToLower(),frases[j].ToLower()) > 0)
+                    {
+                        aux = frases[i];
+                        frases[i] = frases[j];
+                        frases[j] = aux;
+                    }
+                }
+            }
+
+            return frases;
         }
     }
 }
