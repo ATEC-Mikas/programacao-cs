5b87cafc7a78d946c8d0ab7374156350cfa29cf4
Mikas-Atec <miguel.pereira.t0114411@edu.atec.pt>
2019-01-16 23:23:12 +0000

    FT03 iniciada

---
+++ FT03/FT03/Hora.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT03
+{
+    class Hora
+    {
+        private int hora;
+        private int min;
+        private int seg;
+        
+        public int getHora()
+        {
+            return hora;
+        }
+        public bool setHora(int x)
+        {
+            if(x<24 && x>=0)
+            {
+                hora = x;
+                return true;
+            }
+            return false;
+        }
+        public int getMin()
+        {
+            return min;
+        }
+        public bool setMin(int x)
+        {
+            if (x >= 0 && x < 60)
+            {
+                min = x;
+                return true;
+            }
+            return false;
+        }
+        public int getSeg()
+        {
+            return seg;
+        }
+        public bool setSeg(int x)
+        {
+            if (x >= 0 && x < 60)
+            {
+                seg = x;
+                return true;
+            }
+
+            return false;
+        }
+        public string toString()
+        {
+            return (hora.ToString() + ":" + min.ToString() + ":" + seg.ToString());
+        }
+       
+    }
+}
--- FT03/FT03/Program.cs
+++ FT03/FT03/Program.cs
@@ -10,7 +10,9 @@ namespace FT03
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+
+
             Console.ReadKey();
         }
     }
