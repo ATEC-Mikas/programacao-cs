f38aa0208630b17930936d320ba7a6146b736af2
Mikas-Atec <miguel.pereira.t0114411@edu.atec.pt>
2019-01-17 23:25:30 +0000

    ft04 comecada v2 :+1:

--- FT04/FT04/Program.cs
+++ FT04/FT04/Program.cs
@@ -4,16 +4,64 @@ using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 
 namespace FT04
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            ArrayList teste = new ArrayList();
-            
+            ArrayList gerentes =new ArrayList();
+            ArrayList operarios = new ArrayList();
+
+            StreamReader rdgerentes = new StreamReader(@"Gerentes.txt");
+            while(!rdgerentes.EndOfStream)
+            {
+                string linha = rdgerentes.ReadLine();
+                linha = linha.Replace('/', ';');
+                string[] atributos = linha.Split(';');
+                foreach (string teste in atributos)
+                    Console.WriteLine(teste);
+                if(atributos.Length==9)
+                {
+                    gerentes.Add(new Gerente(
+                                             int.Parse(atributos[0]),
+                                             atributos[1],
+                                             atributos[2],
+                                             double.Parse(atributos[3]),
+                                             int.Parse(atributos[4]),
+                                             int.Parse(atributos[5]),
+                                             int.Parse(atributos[6]),
+                                             atributos[7],
+                                             int.Parse(atributos[8])
+                                            ));
+                    Console.WriteLine("Gerente processado.");
+                }
+            }
+            rdgerentes.Close();
+
+            // TODO ARRANJAR OPERARIOS IGUAL A GERENTE
+
+            StreamReader rdoperarios = new StreamReader(@"Operarios.txt");
+            while (!rdoperarios.EndOfStream)
+            {
+                string linha = rdoperarios.ReadLine();
+                linha = linha.Replace('/', ';');
+                string[] atributos = linha.Split(';');
+                operarios.Add(new Operario(
+                                         int.Parse(atributos[0]),
+                                         atributos[1],
+                                         atributos[2],
+                                         double.Parse(atributos[3]),
+                                         int.Parse(atributos[4]),
+                                         int.Parse(atributos[5]),
+                                         int.Parse(atributos[6]),
+                                         atributos[7]
+                                        ));
+            }
+            rdoperarios.Close();
 
 
 
@@ -22,14 +70,45 @@ namespace FT04
             Operario o1 = new Operario(2,"Teste","teste@neves",2,2,3,1990,"Departamento Tijoleiro");
             Console.WriteLine("\n\n"+o1.toString());
 
-            teste.Add(g1);
-            teste.Add(o1);
-            Console.WriteLine();
-            foreach (Object obj in teste)
-                Console.WriteLine(obj);
-            
+            gerentes.Add(g1);
+            operarios.Add(o1);
 
-            Console.WriteLine(teste[0].toString());
+
+            Console.WriteLine("---------------------------------\n\nLog files...\n\n-----------------------------------");
+
+
+            StreamWriter wdgerentes = new StreamWriter(@"Gerentes.txt");
+            foreach(Gerente obj in gerentes)
+            {
+                string linha = obj.getId().ToString() + ";"
+                             + obj.getNome() + ";"
+                             + obj.getEmail() + ";"
+                             + obj.getValorHora().ToString() + ";"
+                             + obj.getDataNascimento().toString() + ";"
+                             + obj.getEspecialidade() + ";"
+                             + obj.getExtensao().ToString()
+                             ;
+                wdgerentes.WriteLine(linha);
+                Console.WriteLine("Gerente guardado.");
+            }
+            wdgerentes.Close();
+
+            StreamWriter wdoperarios = new StreamWriter(@"Operarios.txt");
+            foreach (Operario obj in operarios)
+            {
+                string linha = obj.getId().ToString() + ";"
+                             + obj.getNome() + ";"
+                             + obj.getEmail() + ";"
+                             + obj.getValorHora().ToString() + ";"
+                             + obj.getDataNascimento().toString() + ";"
+                             + obj.getDepartamento()
+                             ;
+                wdoperarios.WriteLine(linha);
+                Console.WriteLine("Operario guardado.");
+            }
+            wdoperarios.Close();
+
+
 
 
             Console.ReadKey();
