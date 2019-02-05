ad4759414d733da3248a43c7c386266a261fa322
Mikas-Atec <miguel.pereira.t0114411@edu.atec.pt>
2019-02-05 21:52:39 +0000

    ft07

---
+++ FT-07/FT-07/Abastecimento.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT_07
+{
+    class Abastecimento
+    {
+        //Atributos & Propriedades
+        public Carro Car { get; set; }
+        public DateTime Data { get; set; }
+        private double _valor;
+        public double Valor { get { return _valor; } set { if (value >= 0) _valor = value; } }
+        private double _litros;
+        public double Litros { get { return _litros; } set { if (value >= 0) _litros = value; } }
+        private double _contakm;
+        public double ContaKM { get { return _contakm; } set { if (value >= 0) _contakm = value; } }
+
+        //Construtores
+        public Abastecimento()
+        {
+            Car = new Carro();
+            Data = DateTime.Today;
+            Valor = 0;
+            Litros = 0;
+            ContaKM = 0;
+        }
+        public Abastecimento(Carro car,DateTime data,double valor,double litros,double contakm)
+        {
+            _valor = -1;
+            _litros = -1;
+            _contakm = -1;
+            this.Car = car;
+            this.Data = data;
+            this.Valor = valor;
+            this.Litros = litros;
+            this.ContaKM = contakm;
+            if (_valor == -1 || _litros == -1 || _contakm == -1)
+                throw (new Exception("Objecto mal criado"));
+        }
+        public Abastecimento(Abastecimento ab)
+        {
+            Car = ab.Car;
+            Data = ab.Data;
+            Valor = ab.Valor;
+            Litros = ab.Litros;
+            ContaKM = ab.ContaKM;
+        }
+        //Funcoes
+        public double calcPrecoLitro()
+        {
+            return Valor / Litros;
+        }
+    }
+}
---
+++ FT-07/FT-07/Carro.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FT_07
+{
+    class Carro
+    {
+        //Atributos && Propriedades
+        public string Matricula { get; set; }
+        private int _ano;
+        public int Ano { get { return _ano; } set { if (value > 1900 && value > DateTime.Today.Year) _ano = value; } }
+        private double _kminiciais;
+        public double KMiniciais { get { return _kminiciais; } set { if (value > 0) _kminiciais = value; } }
+        private string TipoCombustivel { get; set; }
+        public List<Abastecimento> ListaAbastecimento { get; set; }
+
+        //Construtores
+
+        public Carro()
+        {
+            Matricula = "nenhuma";
+            Ano = 2000;
+            KMiniciais = 0;
+            TipoCombustivel = "ar";
+            ListaAbastecimento = new List<Abastecimento>();
+        }
+        public Carro(string mat,int ano,double km,string tipo)
+        {
+            _ano = -1;
+            _kminiciais = -1;
+            ListaAbastecimento = new List<Abastecimento>();
+
+            Matricula = mat;
+            TipoCombustivel = tipo;
+            Ano = ano;
+            KMiniciais = km;
+
+            if (_ano == -1 || _kminiciais == -1)
+                throw new Exception("Objeto construido incorretamente");
+        }
+        public Carro(Carro c)
+        {
+            Matricula = c.Matricula;
+            Ano = c.Ano;
+            TipoCombustivel = c.TipoCombustivel;
+            KMiniciais = c.KMiniciais;
+            ListaAbastecimento = c.ListaAbastecimento;
+        }
+        //Funcoes
+        public bool AdicionarEntrada(Carro car,DateTime data,double valor,double litros,double km)
+        {
+            try
+            {
+                ListaAbastecimento.Add(new Abastecimento(car, data, valor, litros, km));
+                return true;
+            } catch(Exception)
+            {
+                return false;
+            }
+        }
+        public bool RemoverEntrada(int pos)
+        {
+            if (pos >= 0 && pos<=ListaAbastecimento.Count)
+            {
+                ListaAbastecimento.Remove(ListaAbastecimento[pos]);
+                return true;
+            }
+            return false;
+        }
+
+        public double CalcConsumoMedio()
+        {
+
+            return 0;
+        }
+        public double TotalGastoEmCombustivel()
+        {
+            double x=0;
+            foreach(Abastecimento a in ListaAbastecimento)
+            {
+                x += a.Valor;
+            }
+            return x;
+        }
+        public int QuantasSubidasCombustivel()
+        {
+            int x = 0;
+            if(ListaAbastecimento.Count>2)
+                for(int i=0;i<ListaAbastecimento.Count;i++)
+                    if(ListaAbastecimento[i].Valor<ListaAbastecimento[i+1].Valor)
+                        x++;
+
+            return x;
+        }
+        public double DiferencaCombustivel()
+        {
+            if (ListaAbastecimento.Count > 2)
+                return Math.Abs(ListaAbastecimento[0].Valor - ListaAbastecimento[ListaAbastecimento.Count - 1].Valor);
+            return 0;
+        }
+    }
+}
