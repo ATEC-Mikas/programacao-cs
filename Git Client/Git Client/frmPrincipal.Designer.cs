3a35a38d426bc78d491996149466205316e95894
CIBER\t0114411 <SLFRPO119PC07>
2019-02-28 22:40:03 +0000

    teste de commit via git client do mikas

---
+++ Git Client/Git Client/frmPrincipal.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace Git_Client
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ficheiroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.AbrirFolder = new System.Windows.Forms.ToolStripMenuItem();
+            this.EstadoGit = new System.Windows.Forms.ToolStripMenuItem();
+            this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.AddAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtCommit = new System.Windows.Forms.TextBox();
+            this.btnCommit = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ficheiroToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ficheiroToolStripMenuItem
+            // 
+            this.ficheiroToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AbrirFolder,
+            this.EstadoGit,
+            this.AddAll});
+            this.ficheiroToolStripMenuItem.Name = "ficheiroToolStripMenuItem";
+            this.ficheiroToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.ficheiroToolStripMenuItem.Text = "Ficheiro";
+            // 
+            // AbrirFolder
+            // 
+            this.AbrirFolder.Name = "AbrirFolder";
+            this.AbrirFolder.Size = new System.Drawing.Size(180, 22);
+            this.AbrirFolder.Text = "Abrir";
+            this.AbrirFolder.Click += new System.EventHandler(this.AbrirFolder_Click);
+            // 
+            // EstadoGit
+            // 
+            this.EstadoGit.Name = "EstadoGit";
+            this.EstadoGit.Size = new System.Drawing.Size(180, 22);
+            this.EstadoGit.Text = "Estado";
+            this.EstadoGit.Click += new System.EventHandler(this.EstadoGit_Click);
+            // 
+            // FolderBrowser
+            // 
+            this.FolderBrowser.Description = "Description";
+            this.FolderBrowser.ShowNewFolderButton = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(0, 27);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(430, 398);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // AddAll
+            // 
+            this.AddAll.Name = "AddAll";
+            this.AddAll.Size = new System.Drawing.Size(180, 22);
+            this.AddAll.Text = "Add All";
+            this.AddAll.Click += new System.EventHandler(this.AddAll_Click);
+            // 
+            // txtCommit
+            // 
+            this.txtCommit.Location = new System.Drawing.Point(451, 27);
+            this.txtCommit.Multiline = true;
+            this.txtCommit.Name = "txtCommit";
+            this.txtCommit.Size = new System.Drawing.Size(337, 103);
+            this.txtCommit.TabIndex = 4;
+            // 
+            // btnCommit
+            // 
+            this.btnCommit.Location = new System.Drawing.Point(451, 137);
+            this.btnCommit.Name = "btnCommit";
+            this.btnCommit.Size = new System.Drawing.Size(75, 23);
+            this.btnCommit.TabIndex = 5;
+            this.btnCommit.Text = "Commit";
+            this.btnCommit.UseVisualStyleBackColor = true;
+            this.btnCommit.Click += new System.EventHandler(this.btnCommit_Click);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCommit);
+            this.Controls.Add(this.txtCommit);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmPrincipal";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ficheiroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem AbrirFolder;
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
+        private LibGit2Sharp.Repository Repo;
+        private System.Windows.Forms.ToolStripMenuItem EstadoGit;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ToolStripMenuItem AddAll;
+        private System.Windows.Forms.TextBox txtCommit;
+        private System.Windows.Forms.Button btnCommit;
+    }
+}
+
---
+++ Git Client/Git Client/frmPrincipal.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using LibGit2Sharp;
+
+namespace Git_Client
+{
+    public partial class frmPrincipal : Form
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void AbrirFolder_Click(object sender, EventArgs e)
+        {
+            if(DialogResult.OK == FolderBrowser.ShowDialog())
+            {
+                try
+                {
+                    foreach(string n in Directory.GetDirectories(FolderBrowser.SelectedPath))
+                    {
+                        if (Repository.IsValid(n))
+                        {
+                            
+                            MessageBox.Show("Sucesso!","Git encontrado");
+                            Repo = new Repository(n);
+                            FolderBrowser.SelectedPath = String.Empty;
+                            EstadoGit.PerformClick();
+                            break;
+                        }
+                    }
+                } catch(Exception erro)
+                {
+                    MessageBox.Show("Erro! " + erro.Message.ToString());
+                }
+            }
+        }
+
+        private void EstadoGit_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            if(Repo!=null)
+            {
+                foreach (StatusEntry t in Repo.RetrieveStatus().Untracked)
+                {
+                    ListViewItem item = new ListViewItem(t.FilePath);
+                    item.BackColor = Color.OrangeRed;
+                    listView1.Items.Add(item);
+                }
+                foreach (StatusEntry t in Repo.RetrieveStatus().Modified)
+                {
+                    ListViewItem item = new ListViewItem(t.FilePath);
+                    item.BackColor = Color.PaleVioletRed;
+                    listView1.Items.Add(item);
+                }
+                foreach (StatusEntry t in Repo.RetrieveStatus().RenamedInIndex)
+                {
+                    ListViewItem item = new ListViewItem(t.FilePath);
+                    item.BackColor = Color.LightGreen;
+                    listView1.Items.Add(item);
+                }
+                foreach (StatusEntry t in Repo.RetrieveStatus().RenamedInWorkDir)
+                {
+                    ListViewItem item = new ListViewItem(t.FilePath);
+                    item.BackColor = Color.LightGreen;
+                    listView1.Items.Add(item);
+                }
+                foreach (StatusEntry t in Repo.RetrieveStatus().Staged)
+                {
+                    ListViewItem item = new ListViewItem(t.FilePath);
+                    item.BackColor = Color.Green;
+                    listView1.Items.Add(item);
+                }
+                foreach (StatusEntry t in Repo.RetrieveStatus().Added)
+                {
+                    ListViewItem item = new ListViewItem(t.FilePath);
+                    item.BackColor = Color.LightGreen;
+                    listView1.Items.Add(item);
+                }
+            } else
+            {
+                MessageBox.Show("Não tem nenhum Repositório aberto","AVISO");
+            }
+
+                
+        }
+
+        private void AddAll_Click(object sender, EventArgs e)
+        {
+            Commands.Stage(Repo, "*");
+            EstadoGit.PerformClick();
+        }
+        
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCommit_Click(object sender, EventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txtCommit.Text))
+            {
+                Signature sign= new Signature(System.Security.Principal.WindowsIdentity.GetCurrent().Name, Environment.MachineName,DateTimeOffset.Now);
+                Repo.Commit(txtCommit.Text, sign, sign);
+            } else
+            {
+                MessageBox.Show("Mensagem de commit inválida!");
+            }
+        }
+    }
+}
